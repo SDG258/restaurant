36ab4d99cb2ae07c1eea7661bf57aca15163dc93
Song Du <nsdu.17ck1@gmail.com>
2024-12-23 23:04:01 +0700

    Add theme user and admin

---
+++ admin/index.php
@@ -0,0 +1,18 @@
+<?php include 'header.php' ?>
+
+<div class="content-wrapper">
+    <section class="content-header">
+        <h1>
+            Dashboard
+        </h1>
+    </section>
+    <section class="content">
+        <div class="box">
+
+            <div class="box-body">
+                Start creating your amazing application!
+            </div>
+        </div>
+    </section>
+</div>
+<?php include "footer.php" ?>
